6644c8e7f829fbd413a09f94f1e161630b569604
DirkEz <dirk.schaafstra@gmail.com>
2022-05-12 11:22:10 +0200

    Started

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title></title>
+</head>
+<body>
+    <header class="main-header">
+            <div class="rondje">
+                <img id="head-icon" src="img/icon.png" alt="">
+            </div>
+            <div class="text-img">
+                <p>FLIGHTMIGHT</p>
+            </div>
+    </header>
+    <div class="main-blok">
+        <div class="foto">
+
+        </div>
+    </div>
+    <div class="main-onder">
+        <p></p>
+        <p></p>
+    </div>
+    <div class="info">
+    <section class="gallery">
+  <div class="gallery__item">
+    <input type="radio" id="img-1" checked name="gallery" class="gallery__selector"/>
+    <img class="gallery__img" src="img/inf-foto1.jpg" alt=""/>
+    <label for="img-1" class="gallery__thumb"><img src="img/inf-foto1.jpg" alt=""/></label>
+  </div>
+  <div class="gallery__item">
+    <input type="radio" id="img-2" name="gallery" class="gallery__selector"/>
+    <img class="gallery__img" src="https://picsum.photos/id/1039/600/400.jpg" alt=""/>
+    <label for="img-2" class="gallery__thumb"><img src="https://picsum.photos/id/1039/150/100.jpg" alt=""/></label>
+  </div>
+  <div class="gallery__item">
+    <input type="radio" id="img-3" name="gallery" class="gallery__selector"/>
+    <img class="gallery__img" src="https://picsum.photos/id/1057/600/400.jpg" alt=""/>
+    <label for="img-3" class="gallery__thumb"><img src="https://picsum.photos/id/1057/150/100.jpg" alt=""/></label>
+  </div>
+  <div class="gallery__item">
+    <input type="radio" id="img-4" name="gallery" class="gallery__selector"/>
+    <img class="gallery__img" src="https://picsum.photos/id/106/600/400.jpg" alt=""/>
+    <label for="img-4" class="gallery__thumb"><img src="https://picsum.photos/id/106/150/100.jpg" alt=""/></label>
+  </div>
+</section>
+    </div>
+    <div class="info-extra"></div>
+    <footer class="foot"></footer>
+    <script src="js/header.js"></script>
+</body>
+</html>
