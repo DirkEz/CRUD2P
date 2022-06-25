00ae3ce08252edef83df6945089f3d3c32d218af
DirkEz <dirk.schaafstra@gmail.com>
2022-06-25 14:10:58 +0200

    Fixed Searchbar

--- index.php
+++ index.php
@@ -34,22 +34,11 @@
 
       <div class="cover">
         <h1>Discover what's out there.</h1>
-        <form action="search.php" class="flex-form">
+        <form method="post" action="search.php" class="flex-form">
           <label  for="from">
             <i class="ion-location"></i>
           </label>
           <input type="text" class="form__field" name="zoek" />
-          <!-- <select name="bestemmingen" id="bestemmingen"> -->
-          <!-- <option value="start">Waar wil je naartoe?    </option>
-          <option value="best1" name="land">Nederland</option>
-          <option value="best2" name="land">Italie</option>
-          <option value="best3" name="land">Curacao</option>
-          <option value="best4" name="land">Finland</option>
-          <option value="best5" name="land">Egypte</option>
-          <option value="best6" name="land">Amerika</option>
-          <option value="best7" name="land">Nieuw-Zeeland</option> -->
-
-          <!-- </select> -->
           <input type="submit" value="search" />
         </form>
       </div>
--- search.php
+++ search.php
@@ -1,14 +1,37 @@
 <?php 
 
-require_once('php\pages\config\config.php');
+require_once('php/pages/config/config.php')
 
-if (isset($_GET['zoek'])) {
-    $reis = '%'.$_GET['zoek'].'%';
-    $sql = ("SELECT * FROM reizen WHERE hotel LIKE '".$reis."'");
-    $stmt = $connect->prepare($sql);
-    
+?>
 
-  } else {
-    $sql = ("SELECT * FROM reizen");
-  }
-?> 
+
+<!DOCTYPE html>
+<html>
+  <body>
+    <!-- (A) SEARCH FORM
+    <form method="post" action="1-form.php">
+      <h1>SEARCH FOR USERS</h1>
+      <input type="text" name="search" required/>
+      <input type="submit" value="Search"/>
+    </form> -->
+
+    <?php
+    // (B) PROCESS SEARCH WHEN FORM SUBMITTED
+    if (isset($_POST["zoek"])) {
+      // (B1) SEARCH FOR USERS
+      // require "2-search.php";
+      $sql = ("SELECT * FROM `reizen` WHERE `hotel` LIKE ? OR `Land` LIKE ?");
+      $stmt = $connect->prepare($sql);
+      
+      $stmt->execute(["%".$_POST["zoek"]."%", "%".$_POST["zoek"]."%"]);
+      $results = $stmt->fetchAll();
+      // if (isset($_POST["zoek"])) { echo json_encode($results); }
+
+      // (B2) DISPLAY RESULTS
+      if (count($results) > 0) { foreach ($results as $r) {
+        printf("<div>%s - %s</div>", $r["hotel"], $r["Land"]);
+      }} else { echo "No results found"; }
+    }
+    ?>
+  </body>
+</html>
