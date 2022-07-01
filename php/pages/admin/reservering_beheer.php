feac7e43741347c1f8afdeac1b4e3f1af64edd31
DirkEz <dirk.schaafstra@gmail.com>
2022-07-01 12:50:01 +0200

    update

--- php/pages/admin/reservering_beheer.php
+++ php/pages/admin/reservering_beheer.php
@@ -17,7 +17,7 @@ $stmt = $connect->query("SELECT * FROM boekingen");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="../css/style.css"> -->
+    <link rel="stylesheet" href="../css/style.css">
     <title>Boeking Beheer</title>
 </head>
 <body>
@@ -27,10 +27,10 @@ $stmt = $connect->query("SELECT * FROM boekingen");
     <?php  while ($row = $stmt->fetch()) { ?>
     <div class="user">
         <h2>
-        <p><?php echo $row["vliegveld"]; ?> <?php echo "Volwassenen: " . $row['volwassenen'];?> <?php echo "Kinderen: " . $row['kinderen'];?></p>
-        
-            <small> Username: <?php echo $row["username"]; ?></small>  <br>
-            <div class="main_knopje">
+           
+        <p><?php echo "GebruikerID " . $row['gebruikerID'] ?></p> <p><?php echo $row["vliegveld"]; ?> <?php echo "Volwassenen: " . $row['volwassenen'];?> <?php echo "Kinderen: " . $row['kinderen'];?></p>
+    
+            <div class="main_knopje"> <br>
             <div class="knopje">
             <a class="updel" href=" user_update.php?edit=<?php echo $row['gebruikerID']; ?>" class="btn"> <i class="fas fa-edit"></i> Edit </a>
             </div>
